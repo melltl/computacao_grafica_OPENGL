979e84a37634a70541414a590c344a79217711c4
Mel Leão <102246382+melltl@users.noreply.github.com>
2025-03-31 18:14:51 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ import math
 
 def main():
     pygame.init()
-    # project settings
+    # Configurações da janela
     screen_width = 800
     screen_height = 600
-    screen = pygame.display.set_mode((800, 600), DOUBLEBUF | OPENGL)
+    screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
     glClearColor(0.4600, 0.6895, 0.5502, 1)
-    pygame.display.set_caption("Cubo com Transformações: Translação, Escala e Espelhamento")
+    pygame.display.set_caption("Cubo com Reflexo e Transformações")
 
-    gluPerspective(45, (800 / 600), 0.1, 50.0)
+    gluPerspective(45, (screen_width / screen_height), 0.1, 50.0)
     glTranslatef(0, 0, -10)
 
     clock = pygame.time.Clock()
     start_ticks = pygame.time.get_ticks()
-    move_x = -4.0
+    move_x = -2.0
     move_direction = 1
-
     done = False
+
     while not done:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -31,26 +31,28 @@ def main():
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        # Tempo para a escala pulsante
+        # Tempo para a escala pulsante e rotação
         t = (pygame.time.get_ticks() - start_ticks) / 1300.0
         pulsate = 1.0 + 0.5 * math.sin(t * math.pi * 2)
         angle = t * 50
 
-        # Cubo 1: Translação 
+        # Cubo Principal: Rotação, Translação e Escala
         glPushMatrix()
         move_x += 0.02 * move_direction
-        if move_x > -1 or move_x < -4:
+        if move_x > 2 or move_x < -2:
             move_direction *= -1
         glTranslatef(move_x, 0, 0)
         glRotatef(angle, 1, 1, 0)
+        glScalef(pulsate, pulsate, pulsate)
         wire_cube()
         glPopMatrix()
 
-        # Cubo 2: Escala pulsante com espelhamento
+        # Reflexo do Cubo: Inversão no Eixo X
         glPushMatrix()
-        glTranslatef(4, 0, 0)
-        glScalef(-pulsate, pulsate, pulsate)  # Espelhamento no eixo X
+        move_x += 0.02 * move_direction
+        glTranslatef(move_x, -2.5, 0)  # Reflexo abaixo do cubo original
         glRotatef(angle, 1, 1, 0)
+        glScalef(-pulsate, pulsate, pulsate)
         wire_cube()
         glPopMatrix()
 
